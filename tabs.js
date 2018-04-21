c3a5d8e3944c1bf1f2bed1d32fabecb6105eb408
Jan-Markus Langer <kontakt@janmarkuslanger.de>
2018-04-21 19:10:21 +0200

    it is reeeady

--- tabs.js
+++ tabs.js
@@ -1,6 +1,24 @@
 const Tabs = (function(){
 
+  const showTab = function showTab (item, module) {
+    const activeItems = [].slice.call(document.querySelectorAll('[data-tabs-active]'));
+    activeItems[0].removeAttribute('data-tabs-active');
+    activeItems[1].removeAttribute('data-tabs-active');
+    const attribute = item.setAttribute('data-tabs-active', '');
+    module.querySelector(`[data-tabs-content="${item.getAttribute('data-tabs-item')}"]`).setAttribute('data-tabs-active', '');
+  }
 
+  const initModule = function initModule (module) {
+    const barItems = [].slice.call(module.querySelectorAll('[data-tabs-item]'));
+    barItems.forEach(item => {
+      item.addEventListener('click', function(){
+        if(this.getAttribute('data-tabs-active') !== null) {
+          return;
+        }
+        showTab(item, module);
+      })
+    })
+  }
 
   const init = function init () {
     const modules = [].slice.call(document.querySelectorAll('[data-tabs]'));
@@ -8,7 +26,7 @@ const Tabs = (function(){
       return;
     }
     modules.forEach(function(module){
-      
+      initModule(module);
     });
   }
 
@@ -17,6 +35,3 @@ const Tabs = (function(){
   }
 
 }());
-
-
-Tabs.init()
