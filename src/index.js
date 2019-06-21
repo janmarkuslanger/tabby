70bde3c4204f58c360a4b85d4a75b9ffae705087
Jan-Markus Langer <hallo@janmarkuslanger.de>
2019-06-21 07:08:43 +0200

    Clean up index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import { Tabby } from './tabby';
 
-export const version = '3.0.0-beta';
+/**
+ * current version
+ * @type {Number}
+ */
+const version = '3.1';
 
-export const init = (options = {}) => []
+/**
+ * init on tabby element
+ * returns an array of tabby components
+ * @param {Object} options
+ * @return {Array}
+ */
+const init = (options = {}) => []
   .slice
   .call(document.querySelectorAll('[data-tabby]'))
   .map(component => new Tabby(component, options));
+
+export { version, init };
